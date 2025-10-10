91f0b79681f2116705a06abeebb2a0deed748cda
tikawulandari2305-bit <tikawulandari2305@gmail.com>
2025-10-10 13:58:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ if role == "Siswa":
             skor += 1
         
         st.success(f"Skor kamu: {skor}/2")
-hasil.to_csv("hasil_siswa.csv", mode="a", header=False, index=False)
 st.warning("Jawaban kamu sudah direkam!")
 
 # Input sebagai guru
